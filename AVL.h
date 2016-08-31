e71228a2861204ccec38074d49450b2d27ee8beb
Rodrigo França <rodrigo_rjsf@hotmail.com>
2016-08-31 20:01:04 -0300

    Update AVL.h

--- AVL.h
+++ AVL.h
@@ -11,6 +11,7 @@ typedef struct noAvl {
 	char matricula [10], nome [100];
 	float media;
 	int faltas;
+	int status;
 	struct noAvl * esq, * dir;
 	int fatbal;
 }noAVL;
@@ -27,7 +28,9 @@ typedef struct descritor {
 typedef TDescritor * Queue;
 
 noAVL * CriarNo (char codigo[]);
-int buscar (FILE * arq, char matAux[]);
+
+int buscar (FILE * projeto, char matricula[]);
+int carregaArquivo(FILE * projeto, noAVL **raiz, int * status);
 int isEmpty (Queue fila);
 void enqueue (Queue fila, noAVL * n);
 noAVL * dequeue (Queue fila);
@@ -42,14 +45,18 @@ void LLRem(noAVL** r);
 void LRRem (noAVL ** raiz);
 void RRRem(noAVL** r);
 void RLRem (noAVL ** raiz);
-void exibir_no (noAVL *raiz);
-void exibir (noAVL *raiz, char codigo[]);
-void alterar_no (noAVL ** raiz);
-void alterar (noAVL **raiz, char codigo[]);
-void inserir (noAVL **raiz,char codigo[], int *status);
+void exibir_noTodo (FILE * projeto, char matricula[]);
+void exibir_no (FILE * projeto, int pos);
+void exibir (FILE * projeto, noAVL *raiz, char matricula[]);
+void alterar_no (FILE * projeto, char matricula[]);
+void alterar (FILE * projeto, noAVL **raiz, char matricula[]);
+void inserir (FILE * projeto, noAVL **raiz,char codigo[], int *status);
+void inserir_do_Arq (noAVL **raiz,noAVL **aux, int *status);
 void remover(noAVL **raiz, char codigo[], int * cont);
 void removeAll(noAVL **raiz);
 void removerTodos (noAVL **raiz);
+void listarTodos (FILE * projeto);
+void ExibirEmOrdem (FILE * projeto, noAVL **raiz);
 
 
 #endif /* AVL_H_ */
