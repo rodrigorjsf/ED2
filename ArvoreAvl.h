27f2d2aeb638243202816cc26ba4975ba053cb70
Rodrigo França <rodrigo_rjsf@hotmail.com>
2016-09-08 23:48:29 -0300

    Update ArvoreAvl.h

--- ArvoreAvl.h
+++ ArvoreAvl.h
@@ -1,41 +1,30 @@
-/*
- * AVL.h
- *
- *  Created on: 27 de ago de 2016
- *      Author: Rodrigo
- */
+#ifndef AVLARQUIVO_H_
+#define AVLARQUIVO_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <windows.h>
 
-#ifndef AVL_H_
-#define AVL_H_
+/****************
+**  Estruturas **
+*****************/
+
 typedef struct noAvl {
-	char matricula [10], nome [100];
-	float media;
-	int faltas;
-	int status;
+	char matricula [10];
 	struct noAvl * esq, * dir;
 	int fatbal;
 }noAVL;
 
-typedef struct no {
-     noAVL * info;
-    struct no * prox;
-} TNo;
-
-typedef struct descritor {
-    TNo *inicio, *fim;
-} TDescritor;
-
-typedef TDescritor * Queue;
+typedef struct noArq {
+	char matricula [10], nome [100];
+	float media;
+	int faltas;
+	int status;
+}noARQ;
 
 noAVL * CriarNo (char codigo[]);
-
-int buscar (FILE * projeto, char matricula[]);
-int carregaArquivo(FILE * projeto, noAVL **raiz, int * status);
-int isEmpty (Queue fila);
-void enqueue (Queue fila, noAVL * n);
-noAVL * dequeue (Queue fila);
-void initialize (Queue * fila);
-void percorrerPorNivel(noAVL * raiz);
+int buscar (FILE * projeto, char matricula[]);  //funcao de buscar posicao do registro do aluno
 int altura(noAVL* t);
 void LL(noAVL** r);
 void LR(noAVL** r);
@@ -45,18 +34,23 @@ void LLRem(noAVL** r);
 void LRRem (noAVL ** raiz);
 void RRRem(noAVL** r);
 void RLRem (noAVL ** raiz);
-void exibir_noTodo (FILE * projeto, char matricula[]);
-void exibir_no (FILE * projeto, int pos);
-void exibir (FILE * projeto, noAVL *raiz, char matricula[]);
-void alterar_no (FILE * projeto, char matricula[]);
-void alterar (FILE * projeto, noAVL **raiz, char matricula[]);
-void inserir (FILE * projeto, noAVL **raiz,char codigo[], int *status);
-void inserir_do_Arq (noAVL **raiz,noAVL **aux, int *status);
+void exibir_no (char matricula[]);
+void exibir (noAVL *raiz, char matricula[]);
+void alterar_no (char matricula[]);
+void alterar (noAVL *raiz, char matricula[]);
+void inserir (noAVL **raiz,char codigo[], int *status);
+void carregaArquivo(FILE * projeto, noAVL **raiz, int * status);  //carregar arquivo na arvore
+noAVL * mudaTipo (char matricula[]);
+void inserir_do_Arq (noAVL **raiz,char mat[], int *status);
+noAVL * maior(noAVL **raiz);
+void alterarStatus (char mat[]);
+void remover_no(noAVL **raiz);
 void remover(noAVL **raiz, char codigo[], int * cont);
 void removeAll(noAVL **raiz);
 void removerTodos (noAVL **raiz);
-void listarTodos (FILE * projeto);
-void ExibirEmOrdem (FILE * projeto, noAVL **raiz);
+void listarTodos ();
+void exibir_noTodo (noAVL *raiz);
+void ExibirEmOrdem (noAVL *raiz);
+void manutencao ();
 
-
-#endif /* AVL_H_ */
+#endif
